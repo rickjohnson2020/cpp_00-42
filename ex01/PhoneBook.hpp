b0769b1da700c01ebd1436edc70c69cd78c1c50a
Riku Yano <riyano@student.42london.com>
2025-08-25 18:25:57 +0100

    added ex01

---
+++ ex01/PhoneBook.hpp
@@ -0,0 +1,24 @@
+#ifndef PHONEBOOK_HPP
+#define PHONEBOOK_HPP
+
+#include <string>
+
+class PhoneBook
+{
+private:
+	std::string name;
+
+public:
+	PhoneBook(/* args */);
+	~PhoneBook();
+};
+
+PhoneBook::PhoneBook(/* args */)
+{
+}
+
+PhoneBook::~PhoneBook()
+{
+}
+
+#endif
